b021e32bba9eae14fa8af9df4f0ec99797f340b5
Vladimus22 <68963869+Vladimus22@users.noreply.github.com>
2021-08-19 12:50:19 +0300

    upd dimmer

--- ui-buttons/rangeSlider.js
+++ ui-buttons/rangeSlider.js
@@ -1,14 +1,20 @@
-const range = document.querySelector('.range')
-const thumb = document.querySelector('.thumb')
-const track = document.querySelector('.track-inner')
+const range = document.querySelector('.range');
+const thumb = document.querySelector('.thumb');
+const track = document.querySelector('.track-inner');
+
+const valueRange = document.querySelector('.value');
+
+valueRange.innerHTML = range.value;  
 
 const updateSlider = (value) => {
   thumb.style.left = `${value}%`
   thumb.style.transform = `translate(-${value}%, -50%)`
-  track.style.width = `${value}%`
+  track.style.width = `${value}%`  
 }
 
-range.oninput = (e) =>
-  updateSlider(e.target.value)
-
+range.oninput = (e) => {
+  updateSlider(e.target.value);
+  // console.log(range.value);
+  valueRange.innerHTML = range.value;  
+}
 updateSlider(50) // Init value
